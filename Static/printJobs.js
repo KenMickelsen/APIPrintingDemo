abdc1bd19166a9eed44b593fa8aa79f1b4b4b2f7
Ken Mickelsen <kennethmickelsen@gmail.com>
2023-10-17 11:31:25 -0400

    New pages and print jobs from pre-defined files
    Lots of changes, should have had more commits ;)

---
+++ Static/printJobs.js
@@ -0,0 +1,59 @@
+function sendPrintJob(data, callback) {
+    var options = {
+        url: '/send-print-job',
+        type: 'POST',
+        success: function(response) {
+            console.log("Server Response:", response);
+            if (callback) callback(response);
+        }
+    };
+
+    // If data is an instance of FormData, adjust the options for the AJAX call
+    if (data instanceof FormData) {
+        options.data = data;
+        options.contentType = false;
+        options.processData = false;
+    } else {
+        options.data = data;
+    }
+
+    $.ajax(options);
+}
+
+function updateJobsTable() {
+    $.get('/get-jobs', function(data) {
+        var tableBody = $('#jobsTable tbody');
+        tableBody.empty();
+        data.forEach(function(job) {
+            var row = '<tr><td>' + job.timestamp + '</td><td>' + job.printer + '</td><td>' + job.filename + '</td><td>' + job.username + '</td><td>' + job.status + '</td></tr>';
+            tableBody.append(row);
+        });
+    });
+}
+
+$(document).ready(function() {
+    if ($('#jobsTable').length) {
+        updateJobsTable();
+        setInterval(updateJobsTable, 5000);
+    }
+
+    $(".printButton").click(function() {
+        var data = {
+            'filename': $(this).data('file'),
+            'queue': $(this).data('queue'),
+            'username': $(this).data('username'),
+            'copies': $(this).data('copies')
+        };
+        sendPrintJob(data, function(response) {
+            alert(response.status === "success" ? "Job sent successfully!" : "Error: " + response.message);
+        });
+    });
+
+    $("form").submit(function(e) {
+        e.preventDefault();
+        var formData = new FormData(this);
+        sendPrintJob(formData, function(response) {
+            $("#statusMessage").text(response.status === "success" ? "Job sent successfully! Job ID: " + response.data.jobID : "Error: " + response.message);
+        });
+    });
+});
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, Response
+from flask import Flask, render_template, request, jsonify, Response, send_from_directory
 from functools import wraps
 from base64 import b64decode
 from logging.config import dictConfig
@@ -8,6 +8,13 @@ import requests
 import uuid
 import urllib3
 import json
+import os
+
+#Get files from static folder to be available for printing
+def get_static_files():
+    static_dir = "static"
+    valid_extensions = [".pdf"]
+    return [f for f in os.listdir(static_dir) if os.path.splitext(f)[1] in valid_extensions]
 
 def save_jobs_to_file(jobs):
     with open('jobs.json', 'w') as f:
@@ -21,13 +28,10 @@ def load_jobs_from_file():
     except (FileNotFoundError, json.JSONDecodeError):
         return deque([], maxlen=20)  # Return an empty deque with a maximum length of 20 if the file is not found or is empty.
 
-
-#Track print jobs sent
+#Load print job history from jobs.json
 PRINT_JOBS = load_jobs_from_file()
 
-
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
 
 dictConfig({
     'version': 1,
@@ -59,7 +63,7 @@ def requires_auth(f):
 
 def check_auth(username, password):
     """This function is called to check if a username/password combination is valid."""
-    # TODO: Replace with your desired credentials or implement a more robust authentication mechanism
+    # OM Username/pass field for status updates
     return username == 'admin' and password == 'secret'
 
 API_ENDPOINT = "https://192.168.1.166:31990/v1/print"
@@ -68,11 +72,41 @@ API_ENDPOINT = "https://192.168.1.166:31990/v1/print"
 def index():
     return render_template('index.html')
 
+#marketing page to list files for printing
+@app.route('/marketing', methods=['GET'])
+def marketing_page():
+    #Get files in static folder for printing
+    files = get_static_files()
+    return render_template('marketing.html', files=files)
+
+#Portal page for applicants
+@app.route('/application-portal', methods=['GET'])
+def application_portal():
+    return render_template('application_portal.html')
+
+#Settings page for changing values and settings
+@app.route('/settings', methods=['GET'])
+def settings():
+    return render_template('settings.html')
+
 #Send print job
 @app.route('/send-print-job', methods=['POST'])
 def send_print_job():
+    file = request.files.get('file')
+    filename = request.form.get('filename')
+
+    # If a filename is provided, it's a prestored file, so set the file path accordingly
+    if filename:
+        file_path = os.path.join("static", filename)
+        with open(file_path, 'rb') as f:
+            file_content = f.read()
+    elif file:
+        file_content = file.read()
+        filename = file.filename
+    else:
+        return jsonify({"status": "error", "message": "No file provided."})
+
     # Extract data from the form
-    file = request.files['file']
     queue = request.form.get('queue')
     username = request.form.get('username')
     copies = int(request.form.get('copies', 1))
@@ -89,7 +123,7 @@ def send_print_job():
     'jobID': jobID,
     'timestamp': timestamp,
     'printer': queue,
-    'filename': file.filename,
+    'filename': filename,
     'username': username,
     'status': 'Pending'
     }
@@ -110,8 +144,9 @@ def send_print_job():
         'paperSource': paperSource,
         'statusURL': statusURL
     }
+   
     files = {
-        'file': file.read()
+        'file': file_content
     }
     
     response = requests.post(API_ENDPOINT, data=data, files=files, verify=False)  # verify=False is to bypass SSL verification if your local server has a self-signed cert
@@ -140,7 +175,6 @@ def print_job_status():
         break
 
     save_jobs_to_file(list(PRINT_JOBS))  # Save updated list to file
-
 
     # Return a simple acknowledgement response
     return Response("Status received.", status=200)
